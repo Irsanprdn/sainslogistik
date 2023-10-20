4dfa876d7239b1fa3d0394fdfb91911d250a358c
Irsanprdn <muhammadirsanperdana@gmail.com>
2023-10-20 12:40:24 +0700

    Update video.blade.php

--- resources/views/inc/video.blade.php
+++ resources/views/inc/video.blade.php
@@ -1,12 +1,10 @@
-<section id="video" class="video">
+<!-- <section id="video" class="video">
 
     <img src="{{ asset('assets') }}/compro/img/yellow-hexagonal.png" alt="circle-yellow" class="img-fluid position-absolute yellow-circle" style="bottom: -135px;left: -165px;rotate: 120deg;z-index: 200;width: 45%;">
 
     <div class="container position-relative" style="z-index: 300;">
-        <!-- <iframe style="width: 100%; height:500px;" src="https://www.youtube.com/embed/yxyLnGcSsHk"> -->
-        <!-- </iframe> -->
         <video id="video-dua" controls src="{{ asset('assets') }}/uploads/video/COMPANY PROFILE - PT SAINS LOGISTIK INDONESIA.mp4">
         </video>
 
     </div>
-</section>
+</section> -->
