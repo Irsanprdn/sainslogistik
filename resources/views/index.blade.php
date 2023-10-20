6fabeead61b5cc450005ef4170e6756363c42d7f
Irsanprdn <muhammadirsanperdana@gmail.com>
2023-10-20 12:48:29 +0700

    ketinggal versi css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -28,7 +28,7 @@
     <link href="{{ asset('assets') }}/compro/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
     <!-- Template Main CSS File -->
-    <link href="{{ asset('assets') }}/compro/css/style.css?v=3.7" rel="stylesheet">
+    <link href="{{ asset('assets') }}/compro/css/style.css?v=3.8" rel="stylesheet">
 </head>
 
 <body>
