a689597d6f21c3094038510367434d358c9c49f9
Irsanprdn <muhammadirsanperdana@gmail.com>
2023-11-19 23:03:47 +0700

    Update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -28,7 +28,7 @@
     <link href="{{ asset('assets') }}/compro/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
     <!-- Template Main CSS File -->
-    <link href="{{ asset('assets') }}/compro/css/style.css?v=5.5" rel="stylesheet">
+    <link href="{{ asset('assets') }}/compro/css/style.css?v=5.6" rel="stylesheet">
 </head>
 
 <body>
